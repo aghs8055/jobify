2b4acfcb06b92ea86f83d28bca58be86d2f9de04
Ali Ghaffari Sabet <aghs8055@gmail.com>
2024-08-10 16:33:58 +0330

    add user serializer

---
+++ users/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "password2",
+        )
+
+    def validate(data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError("Passwords don't match")
+        return data
+
+    def save(self):
+        user = super().save()
+        user.set_password(self.validated_data["password"])
+        user.save()
+        return user
