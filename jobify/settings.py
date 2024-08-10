203f24bb221bb26d25ce86430f60673e259eb6b0
Ali Ghaffari Sabet <aghs8055@gmail.com>
2024-08-10 13:53:28 +0330

    add user model and change default auth user to this model

--- jobify/settings.py
+++ jobify/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Default user model
+
+AUTH_USER_MODEL = "users.User"
