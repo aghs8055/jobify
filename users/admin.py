75e4856a4db2e055b0b9831cfd10c6f8e9ad3268
Ali Ghaffari Sabet <aghs8055@gmail.com>
2024-08-18 02:47:30 +0330

    add user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User, Employer
+
+
+class EmployerInline(admin.StackedInline):
+    model = Employer
+    can_delete = False
+    verbose_name_plural = "Employer"
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    inlines = (EmployerInline,)
